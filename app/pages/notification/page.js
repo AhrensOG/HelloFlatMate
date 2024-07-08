3af64d557f4e6f849bd480cec2c35653633bba20
DDarioBenitez <reedq2.alt@gmail.com>
2024-07-08 12:39:31 -0300

    Creation of font file, removal of unnecessary CSS files, SEO optimization

--- app/pages/notification/page.js
+++ app/pages/notification/page.js
@@ -1,20 +1,21 @@
-import "../../../styles/notification.css"
+import Image from "next/image"
+import { plus_jakarta } from "@/font"
 
 export default function Notification() {
     return (
-        <div className="notification-page w-screen h-screen flex flex-col justify-center items-center">
+        <div className={`${plus_jakarta.className} notification-page w-screen h-screen flex flex-col justify-center items-center`}>
             <header className="notification-header w-full mt-10">
                 <nav className="notification-nav w-full flex pl-3 items-center">
-                    <button><img src="/back-light.svg"></img></button>
+                    <button><Image src="/back-light.svg" width={20} height={20} alt="Boton para volver" /></button>
                     <h1 className="pl-5 text-xl font-bold text-black">Notificaciones</h1>
                 </nav>
             </header>
             <main className="notification-main grow flex flex-col justify-center items-center gap-4">
-                <img className="notification-icon" src="/notification-big-icon.svg"></img>
-                <section className="notification-section text-center">
-                    <h2 className="notification-title font-semibold">No tienes notificaciones ahora</h2>
-                    <p className="notification-text pt-2 font-light">No tienes notificaciones aun, aqui se <br />
-                        mostraran las<br />
+                <Image className="notification-icon" src="/notification-big-icon.svg" alt="Icono de Notificación Grande" width={100} height={100} />
+                <section className="notification-section text-center text-base">
+                    <h2 className="notification-title font-semibold text-black">No tienes notificaciones ahora</h2>
+                    <p className="notification-text pt-2 font-light text-gris-español">No tienes notificaciones aún. Aquí se <br />
+                        mostrarán las<br />
                         notificaciones cuando tengas alguna.</p>
                 </section>
             </main>
---
+++ font.js
@@ -0,0 +1,11 @@
+import { Abhaya_Libre, Plus_Jakarta_Sans } from "next/font/google";
+
+export const Abhaya = Abhaya_Libre({
+    subsets: ["latin"],
+    weight: ["400", "500", "600", "700", "800"],
+});
+
+export const plus_jakarta = Plus_Jakarta_Sans({
+    subsets: ["latin"],
+    weight: ["200", "300", "400", "500", "700", "800"],
+})
