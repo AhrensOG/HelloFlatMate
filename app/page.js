3c26ac42d815d5d56df536dc188cec7fb7ab9c03
DDarioBenitez <reedq2.alt@gmail.com>
2024-07-13 16:31:37 -0300

    Changes

--- app/page.js
+++ app/page.js
@@ -2,6 +2,4 @@ import Filter from "./components/filter/Filter";
 import FilterSection from "./components/filter/filter_section/FilterSection";
 
 export default function Home() {
-  return <Filter />
-  // return <main className=""></main>;
 }
