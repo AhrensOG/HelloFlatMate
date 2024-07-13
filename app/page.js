dc0d793ca905c92035d0e431f5ec70bdf55499be
AhrensOG <ahrensgabriel09@gmail.com>
2024-07-13 11:55:44 -0300

    Change/delete some classes in navbar and sidebar | Change notification logo

--- app/page.js
+++ app/page.js
@@ -1,10 +1,16 @@
+import Hero from "./components/home/Hero";
 import NavBar from "./components/nav_bar/NavBar";
 
 export default function Home() {
-  return <div className="flex justify-between w-full h-[7vh] px-1.5 pt-1.5">
-    <header className="w-full flex justify-between items-center">
-      <NavBar />
-    </header>
-  </div>
+  return (
+    <div>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <Hero />
+      </main>
+    </div>
+  );
   // return <main className=""><h1>Hello, World!</h1></main>;
 }
