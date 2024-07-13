d533785025a2feaab8af8b68eef794c11a563733
DDarioBenitez <reedq2.alt@gmail.com>
2024-07-13 15:35:09 -0300

    Create Filter view and components of filter

--- app/page.js
+++ app/page.js
@@ -1,4 +1,7 @@
+import Filter from "./components/filter/Filter";
+import FilterSection from "./components/filter/filter_section/FilterSection";
 
 export default function Home() {
-  return <main className=""></main>;
+  return <Filter />
+  // return <main className=""></main>;
 }
