1c50bfa3d41ddc29c00df5630876ddd54e5df75e
DDarioBenitez <reedq2.alt@gmail.com>
2024-07-15 13:06:32 -0300

    Create payment section

---
+++ app/components/payment/Reserve.jsx
@@ -0,0 +1,72 @@
+"use client";
+import { plus_jakarta } from "@/font";
+import Image from "next/image";
+import { motion, AnimatePresence } from "framer-motion";
+
+export default function Reserve({ handleContinue, handleBack }) {
+    console.log(handleContinue, handleBack);
+    return (
+        <motion.section
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5 }}
+            className={`${plus_jakarta.className} w-full h-[90vh] flex flex-col gap-7 p-4`}
+        >
+            <button
+                onClick={handleBack}
+                type="button"
+                className="self-start m-5"
+            >
+                <Image
+                    src={"/payment/back-icon.svg"}
+                    width={24}
+                    height={24}
+                    alt="Boton para regresar"
+                />
+            </button>
+            <div className="flex flex-col gap-5">
+                <div className="flex flex-col gap-3 text-base font-normal text-[#222222]">
+                    <div className="flex justify-between items-center">
+                        <p className="underline underline-offset-2">
+                            500 por mes
+                        </p>
+                        <p>$2,500</p>
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <p className="underline underline-offset-2">
+                            Descuento por larga estadia
+                        </p>
+                        <p className="text-[#008A05]">-$300</p>
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <p className="underline underline-offset-2">
+                            Tarifa de limpieza
+                        </p>
+                        <p>$200</p>
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <p className="underline underline-offset-2">
+                            Tarifa de Servicio
+                        </p>
+                        <p>$0</p>
+                    </div>
+                </div>
+                <div className="h-[1px] bg-[#DDDDDD]"></div>
+                <div className="flex flex-col gap-5 items-center">
+                    <div className="flex justify-between w-full text-base font-normal">
+                        <h3 className="w-100 font-semibold">Total</h3>
+                        <p>$2,400</p>
+                    </div>
+                    <button
+                        onClick={handleContinue}
+                        type="button"
+                        className="self-center text-base font-normal text-white h-[3.25rem] rounded-lg w-[90%] bg-payment-button-gradient hover:bg-payment-button-gradient-hover transition-all duration-300"
+                    >
+                        Resevar
+                    </button>
+                </div>
+            </div>
+        </motion.section>
+    );
+}
---
+++ app/pages/payment/page.jsx
@@ -0,0 +1,50 @@
+"use client";
+import NavBar from "@/app/components/nav_bar/NavBar";
+import AddPaymentMethod from "@/app/components/payment/AddPaymentMethod";
+import PaymentComponent from "@/app/components/payment/PaymentComponent";
+import Reserve from "@/app/components/payment/Reserve";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+export default function Payment() {
+    const [currentStep, setCurrentStep] = useState(1);
+
+    const handleContinue = () => {
+        setCurrentStep(currentStep + 1);
+    };
+
+    const handleBack = () => {
+        setCurrentStep(currentStep - 1);
+    };
+
+    console.log(currentStep);
+    return (
+        <>
+            <header className="w-full flex justify-between items-center h-[7vh] px-1.5 pt-1.5">
+                <NavBar />
+            </header>
+            <main className="flex flex-col items-center">
+                <AnimatePresence mode="wait">
+                    {currentStep === 1 && (
+                        <Reserve
+                            handleContinue={handleContinue}
+                            handleBack={handleBack}
+                        />
+                    )}
+                    {currentStep === 2 && (
+                        <AddPaymentMethod
+                            handleContinue={handleContinue}
+                            handleBack={handleBack}
+                        />
+                    )}
+                    {currentStep === 3 && (
+                        <PaymentComponent
+                            handleContinue={handleContinue}
+                            handleBack={handleBack}
+                        />
+                    )}
+                </AnimatePresence>
+            </main>
+        </>
+    );
+}
