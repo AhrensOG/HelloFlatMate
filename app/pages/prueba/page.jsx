c559806c3681ddd746bb3e638937670a621ddba3
DDarioBenitez <reedq2.alt@gmail.com>
2024-07-29 16:44:08 -0300

    Create Datepicker

---
+++ app/pages/prueba/page.jsx
@@ -0,0 +1,10 @@
+"use client";
+import Datepicker from "@/app/components/property-details/main/reservation/date_picker/DatePicker";
+
+export default function Prueba() {
+  return (
+    <main className="flex justify-center items-center min-h-screen bg-purple-600">
+      <Datepicker />
+    </main>
+  );
+}
