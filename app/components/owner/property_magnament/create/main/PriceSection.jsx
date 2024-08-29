34c553f1f0d5c9fb376cb1d036f424e0b9e88656
DDarioBenitez <reedq2.alt@gmail.com>
2024-08-28 22:36:05 -0300

    Add offer and iva input

--- app/components/owner/property_magnament/create/main/PriceSection.jsx
+++ app/components/owner/property_magnament/create/main/PriceSection.jsx
@@ -5,6 +5,8 @@ export default function PriceSection({ data, setData }) {
     price: 0,
     amountOwner: 0,
     amountHelloflatmate: 0,
+    offer: 0,
+    IVA: 0,
   };
 
   // Inicializa el estado con los datos proporcionados
@@ -39,39 +41,78 @@ export default function PriceSection({ data, setData }) {
 
   return (
     <section className="w-full flex flex-wrap justify-between items-center gap-5">
-      <div className="w-full flex flex-col justify-center gap-1 items-start max-w-52">
-        <label className="font-semibold text-xl" htmlFor="price">
-          Precio Propiedad:{" "}
-        </label>
-        <div className="flex flex-row w-full justify-between items-center border rounded-md p-2 gap-2">
-          <span className="text-xl">€</span>
-          <input
-            className="w-full appearance-none outline-none h-full text-xl number-input-no-appearance"
-            id="price"
-            name="price"
-            value={formData.price || ""}
-            onChange={handleChange}
-            type="number"
-          />
+      <div className="w-full flex flex-wrap justify-between items-end m-3">
+        <div className="w-[45%] flex flex-col justify-center gap-1 items-start max-w-52">
+          <label className="font-semibold text-base" htmlFor="price">
+            Precio Propiedad:{" "}
+          </label>
+          <div className="flex flex-row w-full justify-between items-center border rounded-md p-2 gap-2">
+            <span className="text-xl">€</span>
+            <input
+              className="w-full appearance-none outline-none h-full text-xl number-input-no-appearance"
+              id="price"
+              name="price"
+              value={formData.price || ""}
+              onChange={handleChange}
+              type="number"
+            />
+          </div>
+        </div>
+        <div className="w-[45%] flex flex-col justify-center gap-1 items-start max-w-52">
+          <label
+            className="font-semibold text-base"
+            htmlFor="amountHelloflatmate"
+          >
+            Tarifa Helloflatmate:{" "}
+          </label>
+          <div className="flex flex-row w-full justify-between items-center border rounded-md p-2 gap-2">
+            <span className="text-xl">€</span>
+            <input
+              className="w-full appearance-none outline-none h-full text-xl number-input-no-appearance"
+              id="amountHelloflatmate"
+              name="amountHelloflatmate"
+              value={formData.amountHelloflatmate || ""}
+              onChange={handleChange}
+              type="number"
+            />
+          </div>
         </div>
       </div>
-      <div className="w-full flex flex-col justify-center gap-1 items-start max-w-52">
-        <label className="font-semibold text-xl" htmlFor="amountHelloflatmate">
-          Tarifa Helloflatmate:{" "}
-        </label>
-        <div className="flex flex-row w-full justify-between items-center border rounded-md p-2 gap-2">
-          <span className="text-xl">€</span>
-          <input
-            className="w-full appearance-none outline-none h-full text-xl number-input-no-appearance"
-            id="amountHelloflatmate"
-            name="amountHelloflatmate"
-            value={formData.amountHelloflatmate || ""}
-            onChange={handleChange}
-            type="number"
-          />
+      <div className="w-full flex flex-wrap justify-between items-center m-3">
+        <div className="w-[45%] flex flex-col justify-center gap-1 items-start max-w-52">
+          <label className="font-semibold text-xl" htmlFor="IVA">
+            IVA:{" "}
+          </label>
+          <div className="flex flex-row w-full justify-between items-center border rounded-md p-2 gap-2">
+            <span className="text-lg">%</span>
+            <input
+              className="w-full appearance-none outline-none h-full text-xl number-input-no-appearance"
+              id="IVA"
+              name="IVA"
+              value={formData.IVA || ""}
+              onChange={handleChange}
+              type="number"
+            />
+          </div>
+        </div>
+        <div className="w-[45%] flex flex-col justify-center gap-1 items-start max-w-52">
+          <label className="font-semibold text-lg" htmlFor="offer">
+            Offer:{" "}
+          </label>
+          <div className="flex flex-row w-full justify-between items-center border rounded-md p-2 gap-2">
+            <span className="text-xl">$</span>
+            <input
+              className="w-full appearance-none outline-none h-full text-xl number-input-no-appearance"
+              id="offer"
+              name="offer"
+              value={formData.offer || ""}
+              onChange={handleChange}
+              type="number"
+            />
+          </div>
         </div>
       </div>
-      <div className="w-full flex flex-col justify-center gap-1 items-start max-w-52">
+      <div className="w-full flex flex-col justify-center gap-1 items-start max-w-52 m-3">
         <h2 className="font-semibold text-xl">Neto Propietario: </h2>
         <h2 className="text-xl">€ {formData.amountOwner || 0}</h2>
       </div>
