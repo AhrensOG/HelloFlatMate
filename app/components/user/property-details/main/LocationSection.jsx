dd8ceeb8485921cc1de2d09afd517a6c426ffe34
AhrensOG <ahrensgabriel09@gmail.com>
2024-09-15 08:44:59 -0300

    Add googlemaps dependency | Add geocoding endpoint | Add maps functionaliy

--- app/components/user/property-details/main/LocationSection.jsx
+++ app/components/user/property-details/main/LocationSection.jsx
@@ -1,18 +1,94 @@
-export default function LocationSection() {
+"use client";
+import React, { useCallback, useState, useEffect } from "react";
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+import axios from "axios";
+
+// Función para obtener la latitud y longitud a partir de la dirección
+const getCoordinates = async (address) => {
+  try {
+    // Realizamos la solicitud a nuestro propio endpoint en lugar de Google directamente
+    const response = await axios.get(`/api/maps/geocoding`, {
+      params: {
+        address, // Pasamos la dirección como parámetro
+      },
+    });
+
+    // Si recibimos datos válidos, devolvemos las coordenadas
+    if (response.data && response.data.lat && response.data.lng) {
+      const { lat, lng } = response.data;
+      return { lat, lng };
+    } else {
+      console.error("No se encontraron resultados para la dirección.");
+      return null;
+    }
+  } catch (error) {
+    console.error("Error al obtener las coordenadas:", error);
+    return null;
+  }
+};
+
+// Estilos para el contenedor del mapa
+const containerStyle = {
+  width: "100%",
+  height: "200px",
+};
+
+export default function LocationSection({
+  street,
+  streetNumber,
+  postalCode,
+  city,
+  country,
+}) {
+  const address = `${street} ${streetNumber}, ${postalCode} ${city}, ${country}`;
+  const [location, setLocation] = useState({ lat: 39.4698, lng: -0.3774 }); // Valores por defecto (Madrid)
+  const [loadingLocation, setLoadingLocation] = useState(true); // Estado para manejar el loading de la ubicación
+
+  // Cargar el script de Google Maps con tu API Key
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY, // Asegúrate de almacenar la API Key en las variables de entorno
+  });
+
+  // Al cargar el componente, obtener las coordenadas basadas en la dirección
+  useEffect(() => {
+    const fetchCoordinates = async () => {
+      const coords = await getCoordinates(address);
+      if (coords) {
+        setLocation(coords); // Actualizar la ubicación con las coordenadas obtenidas
+      }
+      setLoadingLocation(false); // Ya no está cargando la ubicación
+    };
+
+    fetchCoordinates();
+  }, [address]); // Solo se ejecuta cuando la dirección cambia
+
+  const onLoad = useCallback(function callback(map) {
+    map.setZoom(16);
+  }, []);
+
+  if (!isLoaded || loadingLocation) {
     return (
-        <section className="flex flex-col gap-3">
-            <h2 className="font-bold text-[1.37rem]">¿Donde Estaras?</h2>
-            <map className="w-full rounded-lg h-[20vh]">
-                <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d194347.47827013538!2d-3.844343464188269!3d40.438098610297125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd422997800a3c81%3A0xc436dec1618c2269!2zTWFkcmlkLCBFc3Bhw7Fh!5e0!3m2!1ses-419!2sar!4v1721089011154!5m2!1ses-419!2sar"
-                    width="100%"
-                    height="100%"
-                    style={{ border: 0, borderRadius: 10 }}
-                    allowFullScreen=""
-                    loading="lazy"
-                    referrerPolicy="no-referrer-when-downgrade"
-                ></iframe>
-            </map>
-        </section>
+      <section className="flex flex-col gap-3">
+        <h2 className="font-bold text-[1.37rem]">¿Dónde Estarás?</h2>
+        <div className="animate-pulse w-full h-[200px] bg-gray-200 rounded-lg">
+          <div className="w-full h-full bg-gray-300"></div>
+        </div>
+      </section>
     );
+  }
+
+  return (
+    <section className="flex flex-col gap-3">
+      <h2 className="font-bold text-[1.37rem]">¿Dónde Estarás?</h2>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={location}
+        zoom={14}
+        onLoad={onLoad}
+      >
+        {/* Marcador en la ubicación */}
+        <Marker position={location} />
+      </GoogleMap>
+    </section>
+  );
 }
