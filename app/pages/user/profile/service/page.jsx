f63965d3e4d3f986df8a4fd06c460475a3d28ee5
AhrensOG <ahrensgabriel09@gmail.com>
2024-10-03 17:56:42 -0300

    Fix deploy bug

--- app/pages/user/profile/service/page.jsx
+++ app/pages/user/profile/service/page.jsx
@@ -4,6 +4,35 @@ import WorkerProfile from "@/app/components/workerPanel/profile/WorkerProfile";
 
 export default function UserServiceProfile() {
   return (
-    <BaseWorkerPanelTemplate children={<WorkerProfile />} section={"home"} />
+    <Suspense fallback={<Loader />}>
+      <BaseWorkerPanelTemplate children={<WorkerProfile />} section={"home"} />
+    </Suspense>
   );
 }
+
+const Loader = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <div className="loader"></div>{" "}
+      {/* Puedes crear una animación CSS para el loader */}
+      <style jsx>{`
+        .loader {
+          border: 8px solid #f3f3f3;
+          border-top: 8px solid #3498db;
+          border-radius: 50%;
+          width: 60px;
+          height: 60px;
+          animation: spin 2s linear infinite;
+        }
+        @keyframes spin {
+          0% {
+            transform: rotate(0deg);
+          }
+          100% {
+            transform: rotate(360deg);
+          }
+        }
+      `}</style>
+    </div>
+  );
+};
