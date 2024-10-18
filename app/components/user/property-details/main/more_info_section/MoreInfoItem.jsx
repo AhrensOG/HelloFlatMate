d033416043a6d7c76316989129e0a706cd21107a
AhrensOG <ahrensgabriel09@gmail.com>
2024-10-18 18:04:11 +0200

    Change some styles in more info section

--- app/components/user/property-details/main/more_info_section/MoreInfoItem.jsx
+++ app/components/user/property-details/main/more_info_section/MoreInfoItem.jsx
@@ -1,29 +1,24 @@
-import { PlusIcon } from "@heroicons/react/20/solid";
+import { MinusIcon, PlusIcon } from "@heroicons/react/20/solid";
 import MoreInfoModal from "./MoreInfoModal";
-import { useState } from "react";
+import { AnimatePresence } from "framer-motion";
 
-export default function MoreInfoItem({ title, body, action }) {
-  const [showModal, setShowModal] = useState();
-
-  const handleShowModal = (body) => {
-    action(body);
-    setShowModal(!showModal);
-  };
-
+export default function MoreInfoItem({ title, body, isOpen, action }) {
   return (
     <article>
       <div className="flex justify-between items-center p-2 text-[#0D171C] bg-[#F7FAFA]">
         <h3 className="font-normal text-lg">{title}</h3>
         <button
-          onClick={() => handleShowModal(body)}
+          onClick={action} // Llama a la función que maneja la apertura
           className="h-10 w-10 text-[#0E155F] p-2 bg-[#E8EDF2] rounded-lg"
           type="button"
         >
-          <PlusIcon />
+          {isOpen ? <MinusIcon /> : <PlusIcon />}
         </button>
       </div>
-
-      {showModal && <MoreInfoModal body={body} action={handleShowModal} />}
+      <AnimatePresence>
+        {/* Solo muestra el modal si este item está abierto */}
+        {isOpen && <MoreInfoModal body={body} action={action} />}
+      </AnimatePresence>
     </article>
   );
 }
--- app/components/user/property-details/main/more_info_section/MoreInfoModal.jsx
+++ app/components/user/property-details/main/more_info_section/MoreInfoModal.jsx
@@ -5,19 +5,12 @@ export default function MoreInfoModal({ body, action }) {
   return (
     <motion.aside
       className="relative p-3"
-      initial={{ opacity: 0, y: -80 }}
+      initial={{ opacity: 0, y: -10 }}
       animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: -80 }}
-      transition={{ duration: 0.8 }}
+      exit={{ opacity: 0, y: -10 }}
+      transition={{ duration: 0.5 }}
     >
-      {/* <button
-        onClick={() => action("")}
-        type="button"
-        className="h-7 w-7 absolute top-1 right-4"
-      >
-        <XMarkIcon />
-      </button> */}
-      <p className="font-normal text-base text-black break-words whitespace-pre-line">
+      <p className="font-normal text-base text-black break-words whitespace-pre-line text-justify">
         {body}
       </p>
     </motion.aside>
