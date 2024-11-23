0ede2eda0cf2267c26961717d96aadd337a5bb7c
AhrensSG <guillermoahrens@gmail.com>
2024-11-23 16:17:47 +0100

    Change some styles

--- app/components/user/history/bedroom/MyBedroomActivitys.jsx
+++ app/components/user/history/bedroom/MyBedroomActivitys.jsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/navigation";
 import ActivityItem from "./my_bedrooms_activity/ActivityItem";
+import { toast } from "sonner";
 
 export default function MyBedroomActivitys({ data }) {
   const route = useRouter();
@@ -33,23 +34,32 @@ export default function MyBedroomActivitys({ data }) {
         <ActivityItem
           title={"Servicios"}
           img={"/my_bedrooms/activitys/services.svg"}
-          action={() => {
-            route.push("/pages/user/services/" + data.id);
-          }}
+          // action={() => {
+          //   route.push("/pages/user/services/" + data.id);
+          // }}
+          action={() =>
+            toast.info("¡Muy pronto podrás solicitar diferentes servicios!")
+          }
         />
         <ActivityItem
           title={"Suministros"}
           img={"/my_bedrooms/activitys/flash.svg"}
-          action={() => {
-            route.push("/pages/user/supplies/" + data.id);
-          }}
+          // action={() => {
+          //   route.push("/pages/user/supplies/" + data.id);
+          // }}
+          action={() =>
+            toast.info("¡Muy pronto podrás ver el estado de tus aportes!")
+          }
         />
         <ActivityItem
           title={"Documentos"}
           img={"/my_bedrooms/activitys/document.svg"}
-          action={() => {
-            route.push("/pages/user/contract/history");
-          }}
+          // action={() => {
+          //   route.push("/pages/user/contract/history");
+          // }}
+          action={() =>
+            toast.info("¡Muy pronto podrás ver tus documentos!")
+          }
         />
       </div>
     </section>
