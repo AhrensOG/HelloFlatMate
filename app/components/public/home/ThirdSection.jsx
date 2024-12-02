2882b04929cd136c752a002831a13c842f98a4a7
AhrensSG <guillermoahrens@gmail.com>
2024-12-02 23:24:08 +0100

    inicio textos y iconos

--- app/components/public/home/ThirdSection.jsx
+++ app/components/public/home/ThirdSection.jsx
@@ -12,7 +12,7 @@ export default function ThirdSection() {
           a encontrar casa y gestionar todo el papeleo.
         </h2>
         <Image
-          src="/home/new_home/first.svg"
+          src="/home/new_home/dormir.gif"
           width={200}
           height={200}
           alt="bot"
@@ -21,14 +21,13 @@ export default function ThirdSection() {
 
       <article className="border-2 rounded-2xl flex flex-col items-center justify-center h-[25rem] gap-4 p-4 w-full max-w-[500px]">
         <h1 className="text-3xl font-bold text-center">
-          Estamos contigo en esto
+        Contratos
         </h1>
         <h2 className="text-lg text-center">
-          Recibe ayuda de un agente en tu idioma, para resolver dudas, ayudarte
-          a encontrar casa y gestionar todo el papeleo.
+        Tener un contrato de alquiler es esencial para garantizar una residencia legal y segura. En hello flat mate, todos nuestros alojamientos se rigen por un modelo contractual basado en el Código Civil, asegurando un equilibrio justo de derechos y responsabilidades tanto para arrendadores como para arrendatarios.  
         </h2>
         <Image
-          src="/home/new_home/second.svg"
+          src="/home/new_home/documento.gif"
           width={200}
           height={200}
           alt="bot"
@@ -37,14 +36,13 @@ export default function ThirdSection() {
 
       <article className="border-2 rounded-2xl flex flex-col items-center justify-center h-[25rem] gap-4 p-4 w-full max-w-[500px]">
         <h1 className="text-3xl font-bold text-center">
-          Estamos contigo en esto
+        Soporte
         </h1>
         <h2 className="text-lg text-center">
-          Recibe ayuda de un agente en tu idioma, para resolver dudas, ayudarte
-          a encontrar casa y gestionar todo el papeleo.
+        Nuestra atención al cliente está disponible de lunes a viernes, de 9:00 a 17:00, para resolver cualquier duda o gestión. Además, ofrecemos un servicio de emergencias 24/7, perfecto para situaciones como olvidar las llaves fuera de horario.
         </h2>
         <Image
-          src="/home/new_home/third.svg"
+          src="/home/new_home/robot-de-chat.gif"
           width={200}
           height={200}
           alt="bot"
@@ -53,14 +51,43 @@ export default function ThirdSection() {
 
       <article className="border-2 rounded-2xl flex flex-col items-center justify-center h-[25rem] gap-4 p-4 w-full max-w-[500px]">
         <h1 className="text-3xl font-bold text-center">
-          Estamos contigo en esto
+        Alojamientos preparados
         </h1>
         <h2 className="text-lg text-center">
-          Recibe ayuda de un agente en tu idioma, para resolver dudas, ayudarte
-          a encontrar casa y gestionar todo el papeleo.
+        Nos encargamos de que cada alojamiento esté limpio y revisado antes de tu llegada, para que disfrutes de una bienvenida cómoda y sin preocupaciones desde el primer día.  
         </h2>
         <Image
-          src="/home/new_home/fourth.svg"
+          src="/home/new_home/escritorio.gif"
+          width={200}
+          height={200}
+          alt="bot"
+        />
+      </article>
+
+      <article className="border-2 rounded-2xl flex flex-col items-center justify-center h-[25rem] gap-4 p-4 w-full max-w-[500px]">
+        <h1 className="text-3xl font-bold text-center">
+        Mantenimiento propio
+        </h1>
+        <h2 className="text-lg text-center">
+        Cuidamos los alojamientos que gestionamos para que siempre estén en buen estado, además siempre revisamos y limpiamos antes de la llegada de nuestros flatmates.
+        </h2>
+        <Image
+          src="/home/new_home/mecanico.gif"
+          width={200}
+          height={200}
+          alt="bot"
+        />
+      </article>
+
+      <article className="border-2 rounded-2xl flex flex-col items-center justify-center h-[25rem] gap-4 p-4 w-full max-w-[500px]">
+        <h1 className="text-3xl font-bold text-center">
+        Alta de suministros
+        </h1>
+        <h2 className="text-lg text-center">
+        Todos nuestros alojamientos, excepto hello Studios, cuentan con luz, agua e Internet WiFi dados de alta antes de tu llegada, para que no tengas que preocuparte por nada. 
+        </h2>
+        <Image
+          src="/home/new_home/lampara-de-escritorio.gif"
           width={200}
           height={200}
           alt="bot"
