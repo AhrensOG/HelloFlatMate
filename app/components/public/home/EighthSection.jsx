887fc1044571ccaf049b9afd3569f96a6172f484
AhrensSG <guillermoahrens@gmail.com>
2024-12-03 13:08:39 +0100

    Change some styles

--- app/components/public/home/EighthSection.jsx
+++ app/components/public/home/EighthSection.jsx
@@ -6,9 +6,9 @@ export default function EightSection() {
   return (
     <section className="bg-white min-h-screen flex flex-col items-center justify-center text-center px-2">
       {/* Ilustración */}
-      <div className="relative w-[300px] h-[200px] sm:w-[550px] sm:h-[350px]">
+      <div className="relative w-[300px] h-[150px] sm:w-[350px] sm:h-[300px]">
         <Image
-          src={"/home/new_home/livingroom.svg"}
+          src={"/home/new_home/demasiado.gif"}
           fill
           alt="livingroom"
           className="object-cover"
@@ -17,13 +17,13 @@ export default function EightSection() {
 
       {/* Texto */}
       <h1 className="text-2xl sm:text-4xl font-semibold text-gray-800 my-10 mt-20">
-        Encuentra tu casa ideal con Spotahome
+        Encuentra tu casa ideal con hello flat mate
       </h1>
 
       {/* Botón */}
       <Link
         href={"/#search"}
-        className="px-6 py-3 sm:text-xl bg-orange-500 text-white font-medium rounded shadow hover:bg-orange-600 transition duration-300"
+        className="px-6 py-3 sm:text-xl bg-violet-300 text-white font-medium rounded shadow hover:bg-violet-400 transition duration-300"
       >
         Empieza a buscar
       </Link>
