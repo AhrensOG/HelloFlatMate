4d1033b794ccfc7df5a69f1edadbf07045d7776b
AhrensOG <ahrensgabriel09@gmail.com>
2025-01-14 12:09:14 +0100

    Delete commentsç

--- utils/cron.js
+++ utils/cron.js
@@ -3,7 +3,6 @@ import axios from "axios";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-// Función que deseas ejecutar periódicamente
 const updateContracts = async () => {
   try {
     const response = await axios.get(`${BASE_URL}/api/cron/update_contracts`);
