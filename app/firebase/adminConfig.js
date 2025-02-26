1191c5bddf6bde87b4cc14da376369a587be8ff6
AhrensOG <ahrensgabriel09@gmail.com>
2025-02-26 14:24:00 +0100

    Change catch | CHange admin config

--- app/firebase/adminConfig.js
+++ app/firebase/adminConfig.js
@@ -1,12 +1,9 @@
 import admin from "firebase-admin";
+import serviceAccount from "./helloflatmateadminsdk.json"
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    }),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
