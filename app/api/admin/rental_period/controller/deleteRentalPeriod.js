b029254e5a88082408fd66efb490573b93ec4438
AhrensOG <ahrensgabriel09@gmail.com>
2025-04-09 14:32:27 +0200

    Format file

--- app/api/admin/rental_period/controller/deleteRentalPeriod.js
+++ app/api/admin/rental_period/controller/deleteRentalPeriod.js
@@ -2,61 +2,79 @@ import { RentalPeriod } from "@/db/init";
 import { NextResponse } from "next/server";
 
 export async function deleteRentalPeriodById(id) {
-    if (!id) {
-        return NextResponse.json({ message: "No id provided" }, { status: 400 })
-    }
+  if (!id) {
+    return NextResponse.json({ message: "No id provided" }, { status: 400 });
+  }
 
+  try {
+    const transaction = await RentalPeriod.sequelize.transaction();
     try {
-        const transaction = await RentalPeriod.sequelize.transaction();
-        try {
-            const rentalPeriod = await RentalPeriod.findByPk(id);
-            if (!rentalPeriod) {
-                return NextResponse.json({ message: "Rental period not found" }, { status: 404 })
-            }
+      const rentalPeriod = await RentalPeriod.findByPk(id);
+      if (!rentalPeriod) {
+        return NextResponse.json(
+          { message: "Rental period not found" },
+          { status: 404 }
+        );
+      }
 
-            await rentalPeriod.destroy({ transaction });
-            await transaction.commit();
-            return NextResponse.json({ rentalPeriod }, { status: 200 })
-        } catch (error) {
-            transaction.rollback();
-            return NextResponse.json({ message: "Error deleting rental period" }, { status: 500 })
-        }
+      await rentalPeriod.destroy({ transaction });
+      await transaction.commit();
+      return NextResponse.json({ rentalPeriod }, { status: 200 });
     } catch (error) {
-        console.log(error)
-        return NextResponse.json({ message: "Error deleting rental period" }, { status: 500 })
+      transaction.rollback();
+      return NextResponse.json(
+        { message: "Error deleting rental period" },
+        { status: 500 }
+      );
     }
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: "Error deleting rental period" },
+      { status: 500 }
+    );
+  }
 }
 
 export async function deleteGroupRentalPeriods(ids) {
-    if (!ids) {
-        return NextResponse.json({ message: "No ids provided" }, { status: 400 })
-    }
+  if (!ids) {
+    return NextResponse.json({ message: "No ids provided" }, { status: 400 });
+  }
 
+  try {
+    const transaction = await RentalPeriod.sequelize.transaction();
     try {
-        const transaction = await RentalPeriod.sequelize.transaction();
-        try {
-            const rentalPeriods = await RentalPeriod.findAll({
-                where: {
-                    id: ids
-                }
-            });
-            if (!rentalPeriods) {
-                return NextResponse.json({ message: "Rental periods not found" }, { status: 404 })
-            }
+      const rentalPeriods = await RentalPeriod.findAll({
+        where: {
+          id: ids,
+        },
+      });
+      if (!rentalPeriods) {
+        return NextResponse.json(
+          { message: "Rental periods not found" },
+          { status: 404 }
+        );
+      }
 
-            await RentalPeriod.destroy({
-                where: {
-                    id: ids
-                },
-                transaction
-            });
-            await transaction.commit();
-            return NextResponse.json({ rentalPeriods }, { status: 200 })
-        } catch (error) {
-            transaction.rollback();
-            return NextResponse.json({ message: "Error deleting rental periods" }, { status: 500 })
-        }
+      await RentalPeriod.destroy({
+        where: {
+          id: ids,
+        },
+        transaction,
+      });
+      await transaction.commit();
+      return NextResponse.json({ rentalPeriods }, { status: 200 });
     } catch (error) {
-        return NextResponse.json({ message: "Error deleting rental periods" }, { status: 500 })
+      transaction.rollback();
+      return NextResponse.json(
+        { message: "Error deleting rental periods" },
+        { status: 500 }
+      );
     }
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error deleting rental periods" },
+      { status: 500 }
+    );
+  }
 }
