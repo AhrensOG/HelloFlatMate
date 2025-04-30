5254dfc6087b4e2cd4be964610663e98324651aa
AhrensOG <ahrensgabriel09@gmail.com>
2025-04-30 13:15:07 +0200

    Delete unnecesary imports

--- app/components/admin/new_panel/FetchDataComponent.jsx
+++ app/components/admin/new_panel/FetchDataComponent.jsx
@@ -1,17 +1,5 @@
-import axios from "axios";
 import React from "react";
 import NewAdminPanel from "./NewAdminPanel";
-import SkeletonLoader from "./SkeletonLoader";
-import { addLeaseOrderToPayments } from "./utils/addLeaseOrderToPayments";
-
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-
-function isEmpty(data) {
-    if (!data) return true;
-    if (Array.isArray(data)) return data.length === 0;
-    if (typeof data === "object") return Object.keys(data).length === 0;
-    return false;
-}
 
 const FetchDataComponent = async () => {
     return <NewAdminPanel />;
